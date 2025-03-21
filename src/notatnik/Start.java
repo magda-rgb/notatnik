d584362b2a6dad7780564de142ae505d3cf299c4
magsxl <magsxl09@gmail.com>
2025-03-21 21:25:49 +0100

    init

---
+++ src/notatnik/Start.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package notatnik;
+
+import frontend.NotatkaFrame;
+
+/**
+ *
+ * @author mszwa
+ */
+public class Start {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NotatkaFrame nf=new NotatkaFrame();
+        nf.setTitle("Start");
+        nf.setSize(400,300);
+        nf.setVisible(true);
+        
+    }
+    
+}
